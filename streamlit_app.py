448c572857f167465f20b9d8a5b056d787048763
nclixby <nickclixby1@gmail.com>
2026-03-26 13:30:19 +0800

    Create streamlit_app.py
    copied from customize your smoothies

---
+++ streamlit_app.py
@@ -0,0 +1,22 @@
+# Import python packages.
+import streamlit as st
+from snowflake.snowpark.context import get_active_session
+from snowflake.snowpark.functions import col
+
+# Write directly to the app.
+st.title(":apple: Customize Your Smoothie :mango:")
+st.write(
+  """Replace this example with your own code!
+  **And if you're new to Streamlit,** check
+  out our easy-to-follow guides at
+  [docs.streamlit.io](https://docs.streamlit.io).
+  """
+)
+
+name_on_order = st.text_input('Name on Smoothie')
+st.write('The name on your Smoothie will be ', name_on_order)
+
+
+session = get_active_session()
+my_dataframe = session.table("smoothies.public.orders").filter(col("ORDER_FILLED") == 0).to_pandas()
+editable_df = st.data_editor(my_dataframe)
